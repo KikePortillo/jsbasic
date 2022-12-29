711ea68cf2a5ffa5fa3cd5c5c393a751960ba370
Kike Portillo <luportillo@fctunca.edu.py>
2022-12-29 14:54:21 -0300

    Practicando js

---
+++ 01/index.js
@@ -0,0 +1,11 @@
+//Este es el mensaje de bienvenida
+console.log("Holi");
+
+/* 
+asi se abre comentario
+y asi se cierra  para el caso de muchas lineas
+*/ 
+
+
+//este es adios
+console.log("Adios");
---
+++ 02/tipos.js
@@ -0,0 +1,21 @@
+//tipos de variables primitivas
+
+//tipos number
+4;
+//string
+"Hola";
+'hola';
+// booleanos
+true;
+false;
+//nulo e indefinido
+null;
+undefined;
+//null, undefined, false, 0
+if (undefined){
+    //si cumple
+    console.log("hola");
+}   else {
+        //sino
+        console.log("NO CUMPLE");
+}
