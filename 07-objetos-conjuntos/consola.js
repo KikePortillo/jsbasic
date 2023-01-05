c675cb2809b45e0d32a770ad8b0c79cb18ac5649
Kike Portillo <luportillo@fctunca.edu.py>
2023-01-05 12:07:34 -0300

    07

--- 07-objetos-conjuntos/consola.js
+++ 07-objetos-conjuntos/consola.js
@@ -2,6 +2,6 @@
 //para mostrar en pantala
 //prompt("Cual es tu edad?");
 //para guardarla en una variable
-//const edad= promp("Cual es tu edad?")
+//const edad= prompt("Cual es tu edad?")
 //para definirla como numero
 //const number_edad = Number(edad)
